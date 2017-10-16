8bf9be46eea07c2a32f1c62c3c25fd7190b9cb99
statu7 <statu7@gmx.de>
2017-10-16 23:17:25 +0200

    Initil commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import SignIn  from './components/SignIn';
+import SignUp from './components/SignUp';
+
+ReactDOM.render(
+	<div>App</div>, document.getElementById('root')
+)
